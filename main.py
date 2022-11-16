f0396827d02e465d14761e9752088187d8a2fcc6
Bhargava Korrapati <korrabhargav@gmail.com>
2022-11-16 21:44:49 +0530

    This is Fast API Project

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'data':'Blog List'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data':id}
